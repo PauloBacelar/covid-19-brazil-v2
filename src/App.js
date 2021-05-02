afc797254a4a0eb72880759c627dbdd976214ec4
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-05-02 08:43:57 -0300

    Changed title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Dados da Pandemia de Covid-19 no Brasil</h1>
+        <img
+          src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/google/274/flag-brazil_1f1e7-1f1f7.png"
+          className="brazil-flag"
+        ></img>
+      </div>
+
+      <div className="states">
+        <img src="https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/SP.png"></img>
+        <h2>Other data</h2>
+      </div>
     </div>
   );
 }
